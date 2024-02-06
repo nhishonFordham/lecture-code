f8e8ead26b8e49215db5ac5311fec656fae05007
Nathan Hishon <nathan.hishon@gmail.com>
2024-02-06 09:47:32 -0500

    2-5 lecture code

---
+++ html-17-19-js-1/js-1/object.js
@@ -0,0 +1,13 @@
+const cat = {
+    name: 'Pinecone',
+    color: 'grey',
+    type: 'long hair',
+    meow: () => {
+        console.log('meow')
+    }
+}
+
+console.log(cat.name)
+cat.meow()
+
+
---
+++ html-17-19-js-1/js-1/typing.js
@@ -0,0 +1,16 @@
+// dynamic typing
+
+// variable a is assigned to a string
+let a = "hello";
+console.log(typeof a);
+
+// variable a is assigned to an integer
+a = 5
+console.log(typeof a)
+
+
+// weak typing
+console.log(1 + '7'); // => '17'
+
+const output = 1 + '7'
+console.log(typeof output)
