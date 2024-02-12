1bc8e81b15f4a6437464b5e78e05a937effda5fa
Nathan Hishon <nathan.hishon@gmail.com>
2024-02-12 15:43:25 -0500

    updates

---
+++ js-2-4/chapter2.js
@@ -0,0 +1,37 @@
+// //statement
+// const name = 'nate';
+//
+// //block
+// // if (name) {
+// //     console.log(name)
+// // }
+//
+//
+// //variables
+//
+// // if (true) {
+// //     var blockVar = 'test'
+// //     const blockConst = 'test2'
+// //     let blockLet = 'test3'
+// // }
+// // //
+// // console.log(blockVar)
+// // console.log(blockConst)
+// // console.log(blockLet)
+//
+//
+// //template literals
+// const string1 = 'yo'
+// const templateLiteral = `${string1} what up?`
+// console.log(templateLiteral)
+//
+// //escape characters
+// const specialChar = "\"quotes\""
+// console.log(specialChar)
+
+
+if ('1' === 1) {
+    console.log('true')
+} else {
+    console.log('false')
+}
